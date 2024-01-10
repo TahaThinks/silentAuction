ada67b93ed2fe682925bcd3b3bb99411a809bad2
TahaThinks <tahaa4587@gmail.com>
2024-01-10 09:34:56 +0400

    Add the bid Funciton

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
 import art
 #HINT: You can call clear() to clear the output in the console.
+bidders = {}
+
+def bid ():
+    bidder_name = input(print("What is your name?: "))
+    bidder_bid = int(input(print("What is your bid?: $ ")))
+    bidders = {bidder_name : bidder_bid}
+
 
 print(art.logo)
 print("Welcome to the secret aution program")
