5d73c3d85539eea38e9b1f1f174d44efbd343b4a
TahaThinks <tahaa4587@gmail.com>
2024-01-10 09:09:08 +0400

    Create travel journal

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
-}
-# 🚨 Don't change the code above 👆
-# TODO-1: Create an empty dictionary called student_grades.
-student_grades = {}
+country = input() # Add country name
+visits = int(input()) # Number of visits
+list_of_cities = eval(input()) # create list from formatted string
 
-# TODO-2: Write your code below to add the grades to student_grades.👇
-for student in student_scores:
-  score = student_scores[student]
-  if score > 90:
-    student_grades[student] = "Outstanding"
-  elif score > 80 and score < 90:
-    student_grades[student] = "Exceeds Expectation"
-  elif score > 70 and score < 80:
-    student_grades[student] = "Acceptable"
-  else:
-    student_grades[student] = "Fail"
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+# Do NOT change the code above 👆
 
- 
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 
+def add_new_country(country, visits, list_of_cities):
+  journal = {
+    "country": country,
+    "visits": visits,
+    "cities": list_of_cities,
+  }
+  travel_log.append(journal)
 
-# 🚨 Don't change the code below 👇
-print(student_grades)
+
+# Do not change the code below 👇
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
